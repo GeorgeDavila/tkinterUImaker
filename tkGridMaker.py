22d7ccac52f09504b211c2430fcd4309c086bb48
George Davila Durendal <GDavila@knights.ucf.edu>
2022-08-11 20:57:47 -0400

    Update tkGridMaker.py

--- tkGridMaker.py
+++ tkGridMaker.py
@@ -3,13 +3,15 @@
 #this assumes each UI element has a unique variable name. So if you want to use the same thing twice, like the label text "hi I'm George" then use two different variables l1 l2 rather than just l1 (probably should do that anyway)
 #The '00' here are placeholders i.e. empty portions of the app
 gridArray = [
-    [ '00', 't1', 'h1' ],
-    [ '00', 'e1', 'b1' ],
+    [ 't1', '00', 'h1' ],
+    [ 'd3', 'd4', 'd5' ],
+    [ 'b2', 'e1', 'b1' ],
     [ 'l1', 'd1', '00' ],
     [ 'l2', 'd2', '00' ],
+    [ 'GD', '00', 'b3' ]
 ]
 
-def gridPlacer(gridArray, emptyEntity = '00'):
+def gridPlacer(gridArray, emptyEntity = '00', default_padx=0, default_pady=0):
     UIentityList = [item for sublist in gridArray for item in sublist] #flattened the grid
 
     while emptyEntity in UIentityList:
@@ -23,10 +25,10 @@ def gridPlacer(gridArray, emptyEntity = '00'):
     for i in UIentityList:
         coordinateTuple = [(index, row.index(i)) for index, row in enumerate(gridArray) if i in row][0] #(row, column) coordinate tuple
         
-        constructionString = constructionString + f"{i}.grid(row={coordinateTuple[0]}, column={coordinateTuple[0]}, padx = 0, pady = 0)" + "\n"
+        constructionString = constructionString + f"{i}.grid(row={coordinateTuple[0]}, column={coordinateTuple[0]}, padx={default_padx}, pady={default_pady})" + "\n"
     
     print(constructionString) #this is actual code output
 
     return constructionString
 
-gridPlacer(gridArray, emptyEntity = '00')
+gridPlacer(gridArray, emptyEntity = '00', default_padx=60, default_pady=60 )
